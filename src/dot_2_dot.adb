c9605e05399b6a5443d689b91133b2bdafca7a2b
rogermc2 <rogermc@iinet.net.au>
2021-09-09 11:12:55 +1000

    Revised

--- src/dot_2_dot.adb
+++ src/dot_2_dot.adb
@@ -1,6 +1,10 @@
 
-with Ada.Text_IO, Ada.Exceptions, Ada.Command_Line;
-with Ada.Strings.Fixed, Ada.Strings.Bounded;
+with Ada.Text_IO;
+with Ada.Exceptions;
+with Ada.Command_Line;
+with Ada.Strings.Fixed;
+with Ada.Strings.Bounded;
+with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 with Config;
 
@@ -9,41 +13,37 @@ with Main_Loop;
 with Types; use Types;
 
 procedure Dot_2_Dot is
-begin
-   --   Get file name from command line
-   --   Output file name is input file name with "-1" added
-   if Ada.Command_Line.Argument_Count /= 1 or else
-     Ada.Strings.Fixed.Index(Ada.Command_Line.Argument(1), ".") = 0 then
-      Ada.Text_IO.Put_Line("Usage: dot2dot file (with extension)");
-   else
-      -- Block used so that files and file names are not global
-      declare
-         use Ada.Text_IO;
-         Input_Name : String  := Ada.Command_Line.Argument (1);
-         Output_Name: String := Ada.Strings.Fixed.Insert
-           (Input_Name, Ada.Strings.Fixed.Index (Input_Name, "."),"-1");
-         Input      :  File_Type;
-         Output     : File_Type;
-      begin
-         Open (Input, In_File, Input_Name);
-         Create (Output, Out_File, Output_Name);
-         Main_Loop (Input, Output);
-         Close (Input);
-         Close (Output);
 
-      exception
-         when Name_Error => Put_Line ("No such file" & Input_Name);
-      end;
-   end if;
+    File_Name : Ada.Strings.Unbounded.Unbounded_String;
+
+    procedure Run is
+        use Ada.Text_IO;
+        Input_Name : String := To_String (File_Name);
+        --   Output file name is input file name with "-1" added
+        Output_Name : String := Ada.Strings.Fixed.Insert
+          (Input_Name, Ada.Strings.Fixed.Index (Input_Name, "."), "-1");
+        Input  :  File_Type;
+        Output : File_Type;
+    begin
+        Open (Input, In_File, Input_Name);
+        Create (Output, Out_File, Output_Name);
+        Main_Loop (Input, Output);
+        Close (Input);
+        Close (Output);
+
+    exception
+        when Name_Error => Put_Line ("No such file" & Input_Name);
+    end Run;
+
+begin
+    --   Get file name from command line
+    Ada.Text_IO.Put ("Dot_2_Dot: Enter File Name: ");
+    File_Name := To_Unbounded_String (Ada.Text_IO.Get_Line);
+    Run;
 
 exception
-   when E : Syntax_Error =>
-      Ada.Text_IO.Put_Line (Ada.Exceptions.Exception_Message (E) &
-                              " expected");
---        Types.Lines_IO.Put_Line (S);
-   when Ada.Text_IO.End_Error =>
-      null;
---        Dot_Tables.Sort (Table);
---        Dot_Tables.Put (Table, Output);
+    when E : Syntax_Error =>
+        Ada.Text_IO.Put_Line (Ada.Exceptions.Exception_Message (E) &
+                                " expected");
 
 end Dot_2_Dot;
